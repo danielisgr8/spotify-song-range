5ea024f16a752938ad0ac5102c5e09cf40f9e496
danielisgr8 <schubertmdaniel@gmail.com>
2019-09-08 17:37:43 -0500

    Added getUpdatedAccessToken that uses refresh token to update access token.

--- server/spotify-networking.js
+++ server/spotify-networking.js
@@ -22,6 +22,25 @@ module.exports.getAccessToken = (code, redirectUri, clientID, clientSecret) => {
     })
 }
 
+module.exports.getUpdatedAccessToken = (refreshToken, clientID, clientSecret) => {
+    const authString = `Basic ${Buffer.from(`${clientID}:${clientSecret}`).toString("base64")}`;
+    const bodyParams = uriEncodeParams({
+        grant_type: "refresh_token",
+        refresh_token: refreshToken
+    });
+    return new Promise((resolve, reject) => {
+        axios.post("https://accounts.spotify.com/api/token", bodyParams, {
+            headers: {
+                "Authorization": authString
+            }
+        })
+        .then((res) => {
+            resolve(res.data.access_token);
+        })
+        .catch((err) => reject(err));
+    });
+}
+
 module.exports.getCurrentSong = async (token) => {
     const response = await axios.get("https://api.spotify.com/v1/me/player/currently-playing", {
         headers: {
