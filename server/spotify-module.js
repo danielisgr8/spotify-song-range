cc3daefda14509dbfd199dd4a63897d3f7ce9fd1
danielisgr8 <schubertmdaniel@gmail.com>
2019-10-03 15:01:23 -0500

    Added null checks.

--- server/spotify-module.js
+++ server/spotify-module.js
@@ -24,7 +24,8 @@ class SpotifyModule {
      */
     _handleError(err) {
         if(err.response.status === 401) {
-            if(err.response.data.error.message === "Invalid access token") {
+            const data = err.response.data;
+            if(data && data.error && data.error.message === "Invalid access token") {
                 throw new SpotifyInvalidTokenError();
             } else {
                 throw new SpotifyExpiredTokenError();
